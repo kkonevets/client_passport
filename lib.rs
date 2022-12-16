3224ba479bf6fff67ca8f3b93ea77b57fc1bc979
Kirill Konevets <kkonevets@gmail.com>
2022-12-16 22:24:00 +0300

    replace String with Vec<u8>

--- lib.rs
+++ lib.rs
@@ -1,20 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(feature = "std")]
-use borsh::{BorshDeserialize, BorshSerialize};
-
 use ink_lang as ink;
-
-#[cfg(feature = "std")]
-#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug)]
-pub struct UserMetadata {
-    /// User INN number
-    inn: u64,
-}
 
 #[ink::contract]
 mod user_passport {
-    use ink_prelude::string::String;
+    use ink_prelude::vec::Vec;
     use ink_storage::traits::SpreadAllocate;
 
     /// User Passport storage
@@ -22,15 +12,15 @@ mod user_passport {
     #[derive(SpreadAllocate)]
     pub struct UserPassport {
         /// User surname
-        surname: String,
+        surname: Vec<u8>,
         /// User name
-        name: String,
+        name: Vec<u8>,
         /// User birthday as Unix Timestamp
         birthday: u64,
         /// Counter of user assets
         assets: ink_storage::Mapping<AccountId, u32>,
-        /// User sercret metadate: INN, ...
-        metadata: String,
+        /// User sercret metadate in BASE64: INN, ...
+        metadata: Vec<u8>,
         /// Marks client's smart contract as active
         active: bool,
         // Store a contract owner
@@ -47,7 +37,7 @@ mod user_passport {
     impl UserPassport {
         /// Constructor that initializes a user passport with the contract as a single asset
         #[ink(constructor)]
-        pub fn new(surname: String, name: String, birthday: u64, metadata: String) -> Self {
+        pub fn new(surname: Vec<u8>, name: Vec<u8>, birthday: u64, metadata: Vec<u8>) -> Self {
             ink_lang::utils::initialize_contract(|contract: &mut Self| {
                 let caller = Self::env().caller();
                 contract.assets.insert(&caller, &1);
@@ -63,9 +53,12 @@ mod user_passport {
 
         /// Get user name and surname if a caller is a contract owner, else only a surname
         #[ink(message)]
-        pub fn get_user_name(&self) -> String {
+        pub fn get_user_name(&self) -> Vec<u8> {
             if Self::env().caller() == self.owner {
-                ink_env::format!("{} {}", &self.surname, &self.name)
+                let mut ret: Vec<u8> = self.surname.to_vec();
+                ret.push(b' ');
+                ret.extend_from_slice(&self.name.to_vec());
+                ret
             } else {
                 self.surname.clone()
             }
@@ -90,7 +83,7 @@ mod user_passport {
 
         /// Get user metadata
         #[ink(message)]
-        pub fn get_metadata(&self) -> Result<String, Error> {
+        pub fn get_metadata(&self) -> Result<Vec<u8>, Error> {
             if Self::env().caller() == self.owner {
                 Ok(self.metadata.clone())
             } else {
@@ -105,11 +98,15 @@ mod user_passport {
     #[cfg(test)]
     mod tests {
         use crate::user_passport::*;
-        use crate::UserMetadata;
-
         use borsh::{BorshDeserialize, BorshSerialize};
         /// Imports `ink_lang` so we can use `#[ink::test]`.
         use ink_lang as ink;
+
+        #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug)]
+        pub struct UserMetadata {
+            /// User INN number
+            inn: u64,
+        }
 
         /// We test a simple use case of our contract.
         #[ink::test]
@@ -120,13 +117,13 @@ mod user_passport {
             println!("base64 encoded metadata: {}", &metadata_encoded);
 
             let mut passport = UserPassport::new(
-                "Иванов".to_owned(),
-                "Иван".to_owned(),
+                "Иванов".into(),
+                "Иван".into(),
                 503556108,
-                metadata_encoded.clone(),
+                metadata_encoded.into(),
             );
 
-            assert_eq!(passport.get_user_name(), "Иванов Иван");
+            assert_eq!(&passport.get_user_name(), "Иванов Иван".as_bytes());
 
             let data = passport.get_metadata().unwrap();
             let decoded: Vec<u8> = base64::decode(data).unwrap();
@@ -137,7 +134,7 @@ mod user_passport {
 
             let account_id: ink_env::AccountId = [0; 32].into();
             passport.owner = account_id;
-            assert_eq!(passport.get_user_name(), "Иванов");
+            assert_eq!(&passport.get_user_name(), "Иванов".as_bytes());
 
             let result = passport.deactivate();
             assert_eq!(result, Err(Error::CallerIsNotAnOwner));
